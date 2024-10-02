18d1042074d1ab41005aa88b7ce69e16b94e5c41
Alexandre_DD <alexandre.delaval.dupuis@gmail.com>
2024-10-02 19:26:37 +0200

    création de la page parametrages et ajout de quelques éléments

---
+++ pages/parametrages.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="u-flex u-flex-direction-column u-align-content-center">
+    <h1>Paramétrages</h1>
+    <h2>Taille du plateau</h2>
+    <h2>Mode</h2>
+    <input type="radio" class="mode">
+    <label for="mode">2048</label>
+    <input type="radio" class="mode">
+    <label for="mode">Infini</label>
+  </div>
+  <div class="u-flex u-justify-content-end">
+    <NuxtLink to="/">Retour</NuxtLink>
+    <NuxtLink to="/jeu"> Confirmer</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+</style>
