1cd63ea967b6713c73d5761d88db16e492a55d6b
Endermax <maxouboulenger80@gmail.com>
2024-10-16 10:02:19 +0200

    Changements de la taille du jeu dans la fenêtre

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,28 +1,26 @@
 import { Scene } from 'phaser';
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
+export class MainMenu extends Scene {
+    constructor () {
         super('MainMenu');
     }
 
-    create ()
-    {
-        this.add.image(512, 384, 'background');
+    create () {
+        let background = this.add.image(0, 0, 'background')
+        .setOrigin(0, 0)  // Place l'origine en haut à gauche
+        .setDisplaySize(window.innerWidth, window.innerHeight);
 
-        this.add.image(512, 300, 'logo');
+        this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'logo')
+            .setOrigin(0.5);
 
-        this.add.text(512, 460, 'Main Menu', {
+        this.add.text(window.innerWidth / 2, window.innerHeight / 2 + 160, 'Menu Principal', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
-
             this.scene.start('Game');
-
         });
     }
 }
