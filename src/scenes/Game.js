170bb20d1427c0bf3b4d547dd056ce6f9bb9e9a4
LeyKoz <lucas.gonthier@etud.u-picardie.fr>
2024-10-16 10:45:17 +0200

    création class player et ajustement du code

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser';
+import Player from '../classes/player.js'
 
 export class Game extends Scene
 {
@@ -8,31 +9,21 @@ export class Game extends Scene
         this.player;
     }
 
-    preload() {
-        this.load.image('player', 'assets/player.png')
+    preload () 
+    {
+        this.load.image('player', '../../public/assets/player.png')
     }
 
     create ()
     {
-        this.player = this.physics.add.sprite(this.scale.width * 0.5, this.scale.height * 0.9, 'player');
-        this.player.setCollideWorldBounds(true);
-        this.cursors = this.input.keyboard.createCursorKeys();
+        this.player = new Player(this, this.scale.width * 0.5, this.scale.height * 0.9, 'player');
 
         this.cameras.main.setBackgroundColor(0x00ff00);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
-        // Réinitialiser la vélocité du joueur
-        this.player.setVelocity(0);
-
-        // Vérifier les touches enfoncées
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);  // Déplace vers la gauche
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);   // Déplace vers la droite
-        }
+        this.player.move(this.cursors);
     }
 }
