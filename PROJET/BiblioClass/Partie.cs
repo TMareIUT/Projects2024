728b97c7b8c2e266755a442c583de7f8c382ae6e
Alphasoa <127762980+Alphasoa@users.noreply.github.com>
2024-11-15 18:21:26 +0100

    changement du puissance 4

---
+++ PROJET/BiblioClass/Partie.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BiblioClass
+{
+    public class Partie
+    {
+        private Grille _plateau;
+        private bool _modeDeJeu; // False IA True 1v1
+
+        //Constructeur
+        public Partie()
+        {
+            _modeDeJeu = false;
+            _plateau = new Grille();
+        }
+
+        public Partie(bool mode, int hauteur, int largeur)
+        {
+            _modeDeJeu = mode;
+            _plateau = new Grille(hauteur,largeur);
+        }
+
+        //Getter
+        public bool modeDeJeu
+        {
+            get
+            {
+                return _modeDeJeu;
+            }
+        }
+
+        public Grille plateau
+        {
+            get
+            {
+                return _plateau;
+            }
+        }
+
+        //Méthode
+        public void ModifierMode(bool Mode)
+        {
+            _modeDeJeu = Mode;
+        }
+
+        public void ModifierPlateau(int hauteur, int largeur)
+        {
+            _plateau=new Grille(hauteur, largeur);
+        }
+        
+        public bool colonnePleine(int colonne)
+        {
+            if (_plateau.remplissage[colonne] < _plateau.hauteur) return false;
+            return true;
+        }
+        public int Eval(Grille _plateau, bool joueur)
+        {
+            int align3true = _plateau.PionsAlign3(true);
+            int align3false = _plateau.PionsAlign3(false);
+            int align2true = _plateau.PionsAlign2(true);
+            int align2false = _plateau.PionsAlign2(false);
+            if (align3true != 0)
+                return -100 * align3true;
+            else if (align3false != 0)
+                return 100 * align3false;
+            else if (align2true != 0)
+                return -10 * align2true;
+            else if (align2false != 0)
+                return 10 * align2false;
+            else return 0;
+        }
+
+        public int MinMax(Grille _plateau, int profondeur, bool joueur)
+        {
+            if (_plateau.Victoire(true)) return -1000;
+            else if (_plateau.Victoire(false)) return 1000;
+            else if (profondeur > 0)
+            {
+                int minPoids = int.MaxValue; // Valeur maximale possible
+                int maxPoids = int.MinValue; // Valeur minimale possible
+                int count = 0;
+                Suivant suivant = new Suivant(_plateau, joueur);
+                if (joueur)
+                {
+                    foreach (Grille grille in suivant.liste)
+                    {
+                        int rec = MinMax(grille, profondeur - 1, !joueur);
+                        if (rec < minPoids)
+                        {
+                            minPoids = rec;
+                            _plateau.meilleurCoup = suivant.colonne[count];
+                        }
+                        count++;
+                    }
+                    return minPoids;
+                }
+                else
+                {
+                    foreach (Grille grille in suivant.liste)
+                    {
+                        int rec = MinMax(grille, profondeur - 1, !joueur);
+                        if (rec > maxPoids)
+                        {
+                            maxPoids = rec;
+                            _plateau.meilleurCoup = suivant.colonne[count];
+                        }
+                        count++;
+                    }
+                    return maxPoids;
+                }
+            }
+            else if (profondeur == 0)
+            {
+                return Eval(_plateau, joueur);
+            }
+            else
+            {
+                Console.WriteLine("Erreur de profondeur");
+                return 0;
+            }
+        }
+    }
+}
+
