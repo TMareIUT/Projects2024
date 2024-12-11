10f6d6e0d056f751c94bcdeb2c9e034162d935de
calexandre <calexandre@8TVLJS3>
2024-12-11 19:07:19 +0100

    Ajout des premiers settings (taille écran, vitesse balle et le paddle (barre)

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,15 @@
 # Dimensions de la fenêtre
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+# Couleurs
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+VIOLET = (148, 0, 211)
+VERT = (0, 255, 0)
+
+# Vitesse initiale
+BALL_SPEED = 5
+PADDLE_SPEED = 10
